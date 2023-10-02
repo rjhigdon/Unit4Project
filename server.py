4612a2aef894493719ac2d8c115f0c53145acbfd
rjhigdon <118936868+rjhigdon@users.noreply.github.com>
2023-10-02 00:33:51 -0700

    done w the minimum nows so let's do a little more maybe

--- server.py
+++ server.py
@@ -16,27 +16,38 @@ def home():
     project_form.update_teams(User.query.get(user_id).teams)
     return render_template("home.html", team_form=team_form, project_form=project_form)
 
-@app.route("/add-team", methods =["POST"])
+@app.route("/add-team", methods=["POST"])
 def add_team():
     team_form = TeamForm()
         
     if team_form.validate_on_submit():
-        print(team_form.team_name.data)
+        team_name = team_form.team_name.data
+        new_team = Team(team_name, user_id)
+        db.session.add(new_team)
+        db.session.commit()
     else:
         print("Form failed to validate.")
     return redirect(url_for("home"))
 
-@app.route("/add-project", methods = ["POST"])
+@app.route("/add-project", methods=["POST"])
 def add_project():
     project_form = ProjectForm()
     project_form.update_teams(User.query.get(user_id).teams)
 
     if project_form.validate_on_submit():
-        print(project_form.project_name.data)
-        print(project_form.description.data)
-        print(project_form.completed.data)
-        print(project_form.team_selection.data)
-    return redirect(url_for("home"))
+        project_name = project_form.project_name.data
+        description = project_form.description.data
+        completed = project_form.completed.data
+        team_selection = project_form.team_selection.data
+        
+        new_project = Project(project_name, description,completed, team_selection)
+        db.session.add(new_project)
+        db.session.commit()
+        
+        return redirect(url_for("home"))
+    else:
+        return redirect(url_for("home"))
+
 
 if __name__ == "__main__":
     connect_to_db(app)
